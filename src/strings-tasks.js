dfd4a57d80aa91687bf17f06c9364ee31f0c407a
Nastassia Romanova <romanova7410020@gmail.com>
2025-10-27 21:17:18 +0300

    fix: add solution getCardId task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -548,8 +548,29 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const suits = ['♣', '♦', '♥', '♠'];
+  const values = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
+  const suit = value.slice(-1);
+  const card = value.slice(0, -1);
+  const suitIndex = suits.indexOf(suit);
+  const cardindex = values.indexOf(card);
+
+  return suitIndex * 13 + cardindex;
 }
 
 module.exports = {
